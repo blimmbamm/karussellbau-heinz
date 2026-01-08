22fea8f5e63b1f021f4db38fa905066e044ac201
Robin Heinz <robin-heinz@web.de>
2026-01-08 12:40:20 +0100

    adds footer component content

--- web/components/footer/Footer.tsx
+++ web/components/footer/Footer.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer className={styles.footer}>Some footer content</footer>;
+  return (
+    <footer className={styles.footer}>
+      <span>&copy; 2025 - Karussellbau Heinz</span>
+      <span className={styles["horizontal-divider"]}>|</span>
+      <Link className={styles.link} href={"/kontakt"}>
+        Impressum und Kontakt
+      </Link>
+    </footer>
+  );
 }
