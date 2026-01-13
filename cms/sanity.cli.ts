a8b684c66dbb0cd6fcfa37edfddecc9d2d5beaa8
Robin Heinz <robin-heinz@web.de>
2026-01-13 08:48:27 +0100

    adds sanity studio appId for subsequent deploys

--- cms/sanity.cli.ts
+++ cms/sanity.cli.ts
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
+    appId: 'a8xp0cqit4x92ul3brk1lnjc',
     autoUpdates: true,
   },
   typegen: {
